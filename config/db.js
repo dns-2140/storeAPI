0fb93fb22c7b7de6e368b4f208b832075cfa2255
dnsdev2140 <davidns2140@gmail.com>
2024-10-12 19:17:37 +0700

    init

---
+++ config/db.js
@@ -0,0 +1,91 @@
+const db = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = db.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+async function fetchProducts() {
+  const [row] = await pool.query('SELECT * FROM products');
+  return row;
+}
+
+async function fetchProduct(id) {
+  const [row] = await pool.query(
+    'SELECT * FROM products where product_id = ?',
+    [id]
+  );
+  return [row];
+}
+
+async function createProduct(data) {
+  console.log('disini');
+  const { name, description, price, quantity } = data;
+  const row = await pool.query(
+    `INSERT INTO products(name, description, price, quantity) VALUES('${name}', '${description}', ${price}, ${quantity})`
+  );
+  console.log(row);
+  return row;
+}
+
+async function updateProduct(id, dataUpdate) {
+  const [targetProduct] = await fetchProduct(id);
+  console.log(targetProduct);
+  if (targetProduct) {
+    const queryArray = [];
+    const queryParameter = [];
+    const { name, description, price, quantity } = dataUpdate;
+    if (name) {
+      queryArray.push('name = ?');
+      queryParameter.push(name);
+    }
+
+    if (description) {
+      queryArray.push('description = ?');
+      queryParameter.push(description);
+    }
+
+    if (price) {
+      queryArray.push('price = ?');
+      queryParameter.push(price);
+    }
+
+    if (quantity) {
+      queryArray.push('quantity = ?');
+      queryParameter.push(quantity);
+    }
+
+    queryParameter.push(+id);
+
+    const queryString = queryArray.join(',');
+    console.log(queryString);
+    console.log(queryParameter);
+    const data = await pool.query(
+      `UPDATE products SET ${queryString} WHERE product_id = ?`,
+      queryParameter
+    );
+
+    const updatedData = await fetchProduct(id);
+    return updatedData;
+  } else {
+    return targetProduct;
+  }
+}
+
+async function deleteProduct(id) {
+  const data = await pool.query('DELETE FROM products WHERE product_id = ?', [
+    id,
+  ]);
+  return data;
+}
+
+module.exports = {
+  fetchProducts,
+  fetchProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const {
+  fetchProducts,
+  fetchProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require('./config/db');
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hitting root route');
+});
+
+app.get('/products', async (req, res) => {
+  const data = await fetchProducts();
+  console.log(data);
+  res.json(data);
+});
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const data = await fetchProduct(id);
+  res.send(data);
+});
+
+app.post('/products', async (req, res) => {
+  const { name, description, price, quantity } = req.body;
+  const data = await createProduct({ name, description, price, quantity });
+  console.log(data);
+  const dataBaru = await fetchProduct(data[0].insertId);
+  res.json(dataBaru);
+});
+
+app.patch('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, description, price, quantity } = req.body;
+  console.log('di route patch');
+  const data = await updateProduct(id, { name, description, price, quantity });
+  res.json(data);
+});
+
+app.delete('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await deleteProduct(id);
+  res.json(data);
+});
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
